1f1b0e281303e36c74672da3ca5f40d5d671d755
Ali-Tamerr <extelx66@gmail.com>
2024-09-20 10:05:48 +0300

    first commit 2

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useNavigate, useLocation } from "react-router-dom";
 import lamboLogo from './imgs/pngwing.com (3).png';
 import searchIcon from './imgs/search-icon.svg';
